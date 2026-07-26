2a14d62e2d858a11b15e9cce5323366ef82e8b10
nishi147 <nsk110761@gmail.com>
2026-07-26 14:43:13 +0530

    gygyhghghgh

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -109,14 +109,14 @@ function RootShell({ children }: { children: ReactNode }) {
       <head>
         <HeadContent />
         {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18264805339" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18348892652" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'AW-18264805339');
+              gtag('config', 'AW-18348892652');
             `,
           }}
         />
